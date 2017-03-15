769846ba94fdd4dc84ffe1dc5f6442d7f7b6e64b
theoryofchao <roger.ti.chao@gmail.com>
2017-03-15 01:39:07 -0400

    testing stylesheets

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use('/public', express.static(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use('/stylesheets',express.static(path.join(__dirname, 'public/stylesheets')));
 
 app.use('/', index);
 app.use('/users', users);
