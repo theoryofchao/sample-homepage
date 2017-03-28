e0a9c5ce35d38982a0a46bd20e49b8bba38387a2
theoryofchao <roger.ti.chao@gmail.com>
2017-03-27 17:32:28 -0700

    added logo and resume

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET resume. */
+router.get('/resume', (req, res, next) => {
+  let tmpFile = "./public/pdf/RogerChaoResume.pdf";
+  fs.readFile(tmpFile, (err, data) => {
+    res.contentType("application/pdf");
+    res.send(data);
+  });
 });
 
 module.exports = router;
