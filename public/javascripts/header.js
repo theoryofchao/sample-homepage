112d03722b03f369b7cb8fe911817f46c80f26a7
theoryofchao <roger.ti.chao@gmail.com>
2017-03-27 22:22:57 -0700

    resizing and adaptative logo

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -3,9 +3,11 @@ $(window).scroll(function(){
     $('header').addClass('sticky');
     $('#logobox').addClass('sticky');
     $('header > nav').addClass('sticky');
+    $('#logobox img').addClass('sticky');
   } else {
     $('header').removeClass('sticky');
     $('#logobox').removeClass('sticky');
     $('header > nav').removeClass('sticky');
+    $('#logobox img').removeClass('sticky');
   }
 });
